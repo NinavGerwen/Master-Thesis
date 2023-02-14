a3eda1bfb323e5fcd8f057411059f9f6b7a44738
ninavgerwen <nina.van.gerwen@gmail.com>
2023-02-14 16:04:04 +0100

    Update

--- Final Code/Functions/chi-square-diff-test.R
+++ Final Code/Functions/chi-square-diff-test.R
@@ -16,11 +16,45 @@ mirt.diff.test <- function(l_0, dataset, k){
             as.data.frame(.) %>%
             mirt(data = ., model = 1, itemtype = "3PL", optimizer = "nlminb", 
                  TOL = .0001, control = list(maxit = 1000),
-                 verbose = TRUE) %>% logLik(.)
+                 verbose = FALSE) %>% logLik(.)
   
   chisqvalue <- 2 * (l_a - l_0)
   
   p.value <- 1 - pchisq(chisqvalue, df = k)
+  
+  return(p.value)
+}
+
+equal.mirt.diff.test <- function(l_0, dataset, k){
+  if(k == 5){
+    l_a <- dataset %>% 
+      as.data.frame(.) %>%
+      mirt(data = ., model = 1, itemtype = "3PL", optimizer = "nlminb", 
+         TOL = .0001, control = list(maxit = 1000),
+         verbose = FALSE, constrain = list(c(3, 7, 11, 15, 19))) %>% logLik(.)
+  }
+  if(k == 10){
+    l_a <- dataset %>% 
+      as.data.frame(.) %>%
+      mirt(data = ., model = 1, itemtype = "3PL", optimizer = "nlminb", 
+           TOL = .0001, control = list(maxit = 1000),
+           verbose = FALSE, constrain = list(c(3, 7, 11, 15, 19, 
+                                               23, 27, 31, 35, 39))) %>% logLik(.)     
+  }
+  if(k == 20){
+    l_a <- dataset %>% 
+      as.data.frame(.) %>%
+      mirt(data = ., model = 1, itemtype = "3PL", optimizer = "nlminb", 
+           TOL = .0001, control = list(maxit = 1000),
+           verbose = FALSE, constrain = list(c(3, 7, 11, 15, 19,
+                                               23, 27, 31, 35, 39,
+                                               43, 47, 51, 55, 59,
+                                               63, 67, 71, 75, 79))) %>% logLik(.)
+  }
+  
+  chisqvalue <- 2 * (l_a - l_0)
+  
+  p.value <- 1 - pchisq(chisqvalue, df = 1)
   
   return(p.value)
 }
--- Final Code/Functions/lr-test-calculation.R
+++ Final Code/Functions/lr-test-calculation.R
@@ -1,4 +1,4 @@
-LR.test <- function(l_0, data, g = 2){ ## for multiple models: add argument model = "1PL"
+LR.test <- function(l_0, data, g = 2, start.val = NULL){ ## for multiple models: add argument model = "1PL"
   
   temp_data <- data %>% as.data.frame(.) %>%
     mutate(split = sample(1:g, nrow(data), replace = TRUE))
@@ -15,7 +15,8 @@ LR.test <- function(l_0, data, g = 2){ ## for multiple models: add argument mode
     ##if(model == "2PL"){
       
       
-      split_LR_values[j] <- ltm(split_set ~ z1, IRT.param = TRUE)$log.Lik
+      split_LR_values[j] <- ltm(split_set ~ z1, IRT.param = TRUE, 
+                                start.val = start.val)$log.Lik
     ##}
     ##if(model == "3PL"){
       
