431f5fc0411c171e8def8f8293db1e16dd3df7ef
NinavGerwen <nina.van.gerwen@gmail.com>
2023-03-07 23:51:56 +0100

    Fix issues

--- Final Code/Functions/multigroup-data-gen.R
+++ Final Code/Functions/multigroup-data-gen.R
@@ -2,13 +2,15 @@
 ## n is here group size, no longer total n
 multigroup.data.gen <- function(n, k, g, method = "theta", model = "2PL"){
   
+  ## We want space for the final simulated matrix
+  data <- matrix(data = NA, nrow = n * g, ncol = k + 1)
+  
+  ## If method is theta, we want differences based on their latent variable
   if(method == "theta"){
-    
-    data <- matrix(data = NA, nrow = n * g, ncol = k + 1)
-    
-    beta <- matrix(data = rep(c(-1, -0.5, 0, 0.5, 1), k), nrow = n, ncol = k, byrow = TRUE)
-    
-    counter <- 1
+
+    ## Therefore, the itemparameters can be the sample for everyone, which
+    ## we define here in matrix beta, alpha, and gamma
+    beta <- matrix(data = rep(c(-1, -0.5, 0, 0.5, 1), k/5), nrow = n, ncol = k, byrow = TRUE)
     
     if(k == 5){
       alpha <- matrix(data = c(0.7, 0.85, 1, 1.15, 1.3), ncol = k,
@@ -29,45 +31,52 @@ multigroup.data.gen <- function(n, k, g, method = "theta", model = "2PL"){
     if(model == "3PL"){
       gamma <- matrix(data = 0.25, ncol = k, nrow = n, byrow = TRUE)
     }
+  
+    ## Now, to generate data we will work with a for loop for every group
     
-  for(i in 1:g){
-
-      theta <- matrix(data = rep((rnorm(n) - (as.numeric(i) - 1)), k), ncol = k)
-      
-      if(model == "2PL"){
-
-      Z <- two.pl(theta = theta, alpha = alpha, beta = beta)
-      
-      }
-      
-      if(model == "3PL"){
+        ## Therefore, we keep a counter for indexing
+        counter <- 1
         
-      Z <- three.pl(theta = theta, alpha = alpha, beta = beta, gamma = gamma)
+        ## Then, for every group:
+        for(i in 1:g){
+            
+            ## Generate latent variables theta in a matrix with differences
+            ## based on the group they are in (group 1 to g)
+            theta <- matrix(data = rep((rnorm(n) - (as.numeric(i) - 1)), k), ncol = k)
+            
+            
+            if(model == "2PL"){
       
-      }
-    
-      data[counter:(counter + n - 1) , 1:k] <- matrix(data = rbinom(n = n * k, size = 1, prob = Z), 
-                                                ncol = k, nrow = n)
-        
-      data[counter:(counter + n - 1), (k + 1)] <- i
-    
-      counter <- counter + n
-    
-    }
+            Z <- two.pl(theta = theta, alpha = alpha, beta = beta)
+            
+            }
+            
+            if(model == "3PL"){
+              
+            Z <- three.pl(theta = theta, alpha = alpha, beta = beta, gamma = gamma)
+            
+            }
+          
+            data[counter:(counter + n - 1) , 1:k] <- matrix(data = rbinom(n = n * k, size = 1, prob = Z), 
+                                                      ncol = k, nrow = n)
+              
+            data[counter:(counter + n - 1), (k + 1)] <- i
+          
+            counter <- counter + n
+          
+          }
   
 
   }
     
   if(method == "param"){
-    
-    data <- matrix(data = NA, nrow = n * g, ncol = k + 1)
-    
+
     counter <- 1
     
     for(i in 1:g){
       theta <- matrix(data = rep((rnorm(n)), k), ncol = k)
       
-      beta <- matrix(data = rep( (c(-1, -0.5, 0, 0.5, 1) - ((as.numeric(i) -  1)/ 2)) , k), 
+      beta <- matrix(data = rep( (c(-1, -0.5, 0, 0.5, 1) - ((as.numeric(i) -  1)/2)) , k/5), 
                                                   nrow = n, ncol = k, byrow = TRUE)
       
       if(k == 5){
